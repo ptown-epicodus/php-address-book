81754297c2d0c8be109ef732e4d77b9d8a86228d
Patrick McGreevy <patrick7490@icloud.com>
2017-02-10 10:10:28 -0800

    Add Contact model

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
     date_default_timezone_set('America/Los_Angeles');
 
     require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../src/Contact.php';
 
     $app = new Silex\Application();
 
---
+++ src/Contact.php
@@ -0,0 +1,45 @@
+<?php
+    class Contact
+    {
+            private $name;
+            private $phone;
+            private $address;
+
+            function __construct($name, $phone, $address)
+            {
+                $this->name = $name;
+                $this->phone = $phone;
+                $this->address = $address;
+            }
+
+            function getName()
+            {
+                return $this->name;
+            }
+
+            function setName($new_name)
+            {
+                $this->name = $new_name;
+            }
+
+            function getPhone()
+            {
+                return $this->phone;
+            }
+
+            function setPhone($new_phone)
+            {
+                $this->phone = $new_phone;
+            }
+
+            function getAddress()
+            {
+                return $this->address;
+            }
+
+            function setAddress($new_address)
+            {
+                $this->address = $new_address;
+            }
+    }
+?>
