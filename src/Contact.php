a4c776e25c4ea5df638eac94c1265651af71a0d5
Patrick McGreevy <patrick7490@icloud.com>
2017-02-10 11:29:19 -0800

    Add routes

--- src/Contact.php
+++ src/Contact.php
@@ -1,45 +1,60 @@
 <?php
     class Contact
     {
-            private $name;
-            private $phone;
-            private $address;
+        private $name;
+        private $phone;
+        private $address;
 
-            function __construct($name, $phone, $address)
-            {
-                $this->name = $name;
-                $this->phone = $phone;
-                $this->address = $address;
-            }
+        function __construct($name, $phone, $address)
+        {
+            $this->name = $name;
+            $this->phone = $phone;
+            $this->address = $address;
+        }
 
-            function getName()
-            {
-                return $this->name;
-            }
+        function getName()
+        {
+            return $this->name;
+        }
 
-            function setName($new_name)
-            {
-                $this->name = $new_name;
-            }
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
 
-            function getPhone()
-            {
-                return $this->phone;
-            }
+        function getPhone()
+        {
+            return $this->phone;
+        }
 
-            function setPhone($new_phone)
-            {
-                $this->phone = $new_phone;
-            }
+        function setPhone($new_phone)
+        {
+            $this->phone = $new_phone;
+        }
 
-            function getAddress()
-            {
-                return $this->address;
-            }
+        function getAddress()
+        {
+            return $this->address;
+        }
 
-            function setAddress($new_address)
-            {
-                $this->address = $new_address;
-            }
+        function setAddress($new_address)
+        {
+            $this->address = $new_address;
+        }
+
+        function save()
+        {
+            array_push($_SESSION['list_of_contacts'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_contacts'];
+        }
+
+        static function deleteAll()
+        {
+            $_SESSION['list_of_contacts'] = array();
+        }
     }
 ?>
